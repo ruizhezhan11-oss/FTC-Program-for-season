d73567f684180e1fb014597997b630a965798a54
ruizhezhan11-oss <ruizhezhan11@gmail.com>
2026-08-22 14:35:48 +0800

    8.22

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BIOBUZZ_SEASON.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BIOBUZZ_SEASON.java
@@ -1,9 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 
@@ -27,6 +33,44 @@ public class BIOBUZZ_SEASON extends LinearOpMode {
         BR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         FR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+    public class SimpleLocalizer {
+        public GoBildaPinpointDriver smallbox;
+        public double x = 0, y = 0, heading = 0;
+
+        public SimpleLocalizer (HardwareMap hardwareMap) {
+            // 讀取在 Robot Configuration 裡設定名稱為 "pinpoint" 的裝置
+            smallbox = hardwareMap.get(GoBildaPinpointDriver.class, "pinpoint");
+
+            // 1. 設定定位輪類型 4Bar
+            smallbox.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+
+            // 2. 設定 Pod 距離機器人旋轉中心的物理距離 (單位：公釐 mm)
+            smallbox.setOffsets(-84.0, -168.0, DistanceUnit.MM);
+            // 3. 設定 Encoder 正反向 (若推動機器人時座標變負數可修改此處)
+            smallbox.setEncoderDirections(
+                    GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                    GoBildaPinpointDriver.EncoderDirection.FORWARD
+            );
+
+            // 4. 重設座標與角度
+            reset();
+        }
+
+        public void reset() {
+            smallbox.resetPosAndIMU(); // 強制歸零
+        }
+
+        public void update() {
+            // 觸發 Pinpoint 讀取與計算
+            smallbox.update();
+
+            // 提取計算好的場地絕對座標
+            Pose2D pose = smallbox.getPosition();
+            x = pose.getX(DistanceUnit.INCH);      // 轉換為 吋
+            y = pose.getY(DistanceUnit.INCH);      // 轉換為 吋
+            heading = pose.getHeading(AngleUnit.RADIANS); // 取得弧度角
+        }
     }
     @Override
     public void runOpMode(){
