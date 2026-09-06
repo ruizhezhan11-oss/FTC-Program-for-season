c00ba44e80c6918b31c2f12a97c25cf6eaafc6e1
ruizhezhan11-oss <ruizhezhan11@gmail.com>
2026-09-06 12:43:34 +0800

    9.6

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/STAFF_v2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/STAFF_v2.java
@@ -10,11 +10,13 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class STAFF_v2 extends LinearOpMode {
     DcMotor FL,FR,BL,BR,intake;
+    Servo angleservo;
     double targetRPM = 3750;//Shooter固定參數
     double CPR = 28;//Shooter固定參數
     boolean autoFireOn = false;//自動需要
     DcMotorEx shooterLeft,shooterRight;
     public void init_hardware(){
+        angleservo=hardwareMap.get(Servo.class,"angleservo");
         BL=hardwareMap.get(DcMotor.class,"BL");
         BR=hardwareMap.get(DcMotor.class,"BR");
         FL=hardwareMap.get(DcMotor.class,"FL");
@@ -22,6 +24,7 @@ public class STAFF_v2 extends LinearOpMode {
         intake=hardwareMap.get(DcMotor.class,"intake");
         shooterLeft=hardwareMap.get(DcMotorEx.class,"shooterLeft");
         shooterRight=hardwareMap.get(DcMotorEx.class,"shooterRight");
+        angleservo.setDirection(Servo.Direction.FORWARD);
         BL.setDirection(DcMotor.Direction.REVERSE);
         BR.setDirection(DcMotor.Direction.FORWARD);
         FL.setDirection(DcMotor.Direction.REVERSE);
@@ -36,12 +39,21 @@ public class STAFF_v2 extends LinearOpMode {
         shooterLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         shooterRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
+    public void init_angleServo(){
+        double claw_Intake= (35.5);
+        double claw_Shoot=(65.5);
+        angleservo.setPosition(0);
+
+    }
     @Override
     public void runOpMode(){
         init_hardware();
+        init_angleServo();
+        telemetry.update();
         waitForStart();
        while (opModeIsActive()) {
            double ticksPerSecond = (targetRPM / 60.0) * CPR;
+           telemetry.addData("Servo角度",(angleservo.getPosition())*180);
            telemetry.addData("目標轉速：",targetRPM);
            double y=-gamepad1.left_stick_y;
            double x= gamepad1.left_stick_x;
