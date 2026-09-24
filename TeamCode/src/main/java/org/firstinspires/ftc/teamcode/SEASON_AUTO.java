34a2587037c75e4a2e5ef8064d60c1f4320e85ea
J.Zan <ruizhezhan11@gmail.com>
2026-09-24 18:04:42 +0800

    0165195

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SEASON_AUTO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SEASON_AUTO.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="TEAM_AUTO")
 public abstract class  SEASON_AUTO extends LinearOpMode {
+    private GoBildaPinpointDriver Pinpoint;
     private DcMotor BL,BR,FL,FR;
     public void init_hardware(){
         BL=hardwareMap.get(DcMotor.class,"BL");
@@ -19,11 +20,21 @@ public abstract class  SEASON_AUTO extends LinearOpMode {
         BR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         FR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
+        Pinpoint=hardwareMap.get(GoBlidaPinpointDriver.class,"Pinpoint");
+    }
+    private void init_xy(){
+        pinpoint1.setEncoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD);
+        pinpoint1.setOffsets(111, 111, DistanceUnit.MM);
+        pinpoint1.setEncoderDirections(
+                GoBildaPinpointDriver.EncoderDirection.FORWARD,
+                GoBildaPinpointDriver.EncoderDirection.FORWARD
+        );
+        pinpoint1.resetPosAndIMU();
     }
     @Override
     public void runOpMode(){
         init_hardware();
+        init_xy();
         waitForStart();
         while(opModeIsActive()){
 
